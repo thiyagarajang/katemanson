6ceab06555455b43737bd820a488191253f712a5
David Dunwoody <david.dunwoody@gmail.com>
2008-12-30 16:24:18 +0000

    add spec for application layout

---
+++ spec/views/layouts/application.html.erb_spec.rb
@@ -0,0 +1,15 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+# From http://rubyforge.org/pipermail/rspec-users/2007-October/004026.html
+describe 'layouts/application' do
+  it 'should yield' do
+    render :text => "<div class='yieldtest'>yielded</div>"
+    response.should have_tag('div.yieldtest', 'yielded')
+  end
+
+  it "should set the page title to 'Kate Manson : @nav_title'" do
+    assigns[:nav_title] = 'Location'
+    render 'content/home', :layout => 'application'
+    response.should have_tag('title', 'Kate Manson : Location')
+  end
+end
