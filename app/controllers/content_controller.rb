5999457c94e7e1c111aa40cc48be4a9e5bf88765
David Dunwoody <david.dunwoody@gmail.com>
2008-12-30 16:04:38 +0000

    update title based on location

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -1,4 +1,6 @@
 class ContentController < ApplicationController
+  before_filter :set_nav_context
+
   def home
   end
 
@@ -12,5 +14,11 @@ class ContentController < ApplicationController
   end
 
   def blog
+  end
+
+  private
+
+  def set_nav_context
+    @nav_title = action_name.capitalize
   end
 end
--- spec/controllers/content_controller_spec.rb
+++ spec/controllers/content_controller_spec.rb
@@ -2,36 +2,85 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe ContentController do
   describe 'responding to GET :home' do
-    it 'should render home.html.erb' do
+    def do_get
       get :home
+    end
+
+    it "should set @nav_title to 'Home'" do
+      do_get
+      assigns[:nav_title].should == 'Home'
+    end
+
+    it 'should render home.html.erb' do
+      do_get
       response.should render_template('content/home')
     end
   end
 
+
   describe 'responding to GET :gallery' do
-    it 'should render gallery.html.erb' do
+    def do_get
       get :gallery
+    end
+
+    it "should set @nav_title to 'Gallery'" do
+      do_get
+      assigns[:nav_title].should == 'Gallery'
+    end
+
+    it 'should render gallery.html.erb' do
+      do_get
       response.should render_template('content/gallery')
     end
   end
 
+
   describe 'responding to GET :contact' do
-    it 'should render contact.html.erb' do
+    def do_get
       get :contact
+    end
+
+    it "should set @nav_title to 'Contact'" do
+      do_get
+      assigns[:nav_title].should == 'Contact'
+    end
+
+    it 'should render contact.html.erb' do
+      do_get
       response.should render_template('content/contact')
     end
   end
 
+
   describe 'responding to GET :emporium' do
-    it 'should render emporium.html.erb' do
+    def do_get
       get :emporium
+    end
+
+    it "should set @nav_title to 'Emporium'" do
+      do_get
+      assigns[:nav_title].should == 'Emporium'
+    end
+
+    it 'should render emporium.html.erb' do
+      do_get
       response.should render_template('content/emporium')
     end
   end
 
+
   describe 'responding to GET :blog' do
-    it 'should render blog.html.erb' do
+    def do_get
       get :blog
+    end
+
+    it "should set @nav_title to 'Blog'" do
+      do_get
+      assigns[:nav_title].should == 'Blog'
+    end
+
+    it 'should render blog.html.erb' do
+      do_get
       response.should render_template('content/blog')
     end
   end
