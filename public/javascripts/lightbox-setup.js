125b454cf13175b4c983fe2b13ee69d6a6489f7f
David Dunwoody <david.dunwoody@gmail.com>
2009-01-03 15:58:38 +0000

    fix 404s from lightbox

--- public/javascripts/lightbox-setup.js
+++ public/javascripts/lightbox-setup.js
@@ -4,11 +4,17 @@ $(function(){
         show_linkback: false,
         show_info: true,
         files: {
+            css: {
+                lightbox: '/stylesheets/jquery.lightbox.packed.css'
+            },
             images: {
-                prev: 'images/lightbox/prev.gif',
-                next: 'images/lightbox/next.gif',
-                blank: 'images/lightbox/blank.gif',
-                loading: 'images/lightbox/loading.gif'
+                prev: '/images/lightbox/prev.gif',
+                next: '/images/lightbox/next.gif',
+                blank: '/images/lightbox/blank.gif',
+                loading: '/images/lightbox/loading.gif'
+            },
+            js: {
+                lightbox: '/javascripts/jquery.lightbox.packed.js'
             }
         }
     });
