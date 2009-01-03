78df55ee99fa1ca0e18ffc1a125dcdce5d53faae
David Dunwoody <david.dunwoody@gmail.com>
2009-01-03 19:51:04 +0000

    added capistrano deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,24 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, 'katemanson.co.uk'
+set :repository,  'git://github.com/ddunwoody/katemanson.git'
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "/var/www/#{application}"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
 # set :scm, :subversion
+set :scm, :git
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+server '208.75.87.111', :app, :web, :db, :primary => true
+
+
+namespace :passenger do
+  desc "Restart Application"
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
+
+after :deploy, "passenger:restart"
