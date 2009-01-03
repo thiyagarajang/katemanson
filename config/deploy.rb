d5424efd55337fc85e57f69cb41342394c0b2ad6
David Dunwoody <david.dunwoody@gmail.com>
2009-01-03 19:57:10 +0000

    update deployment script with code from http://tomcopeland.blogs.com/juniordeveloper/2008/05/mod_rails-and-c.html

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,11 +14,15 @@ set :scm, :git
 server '208.75.87.111', :app, :web, :db, :primary => true
 
 
-namespace :passenger do
-  desc "Restart Application"
-  task :restart do
+# from http://tomcopeland.blogs.com/juniordeveloper/2008/05/mod_rails-and-c.html
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
     run "touch #{current_path}/tmp/restart.txt"
   end
-end
 
-after :deploy, "passenger:restart"
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+end
