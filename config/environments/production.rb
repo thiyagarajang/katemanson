a88f55c4f1955864a61f16b0bddb138201062859
David Dunwoody <david.dunwoody@gmail.com>
2009-01-03 18:52:27 +0000

    configure destination addresses

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,5 +20,4 @@ config.action_controller.perform_caching             = true
 # Enable serving of images, stylesheets, and javascripts from an asset server
 # config.action_controller.asset_host                  = "http://assets.example.com"
 
-# Disable delivery errors, bad email addresses will be ignored
-# config.action_mailer.raise_delivery_errors = false
+CONTACT_EMAIL = 'pinkhalf@gmail.com'
